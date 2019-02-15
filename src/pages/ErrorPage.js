988148c9e46257acfe56a6ccabe7715bc56c7c2b
charminz <indrek.toomsalu@gmail.com>
2019-02-15 23:16:34 +0200

    Initial error page

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+	render() {
+		return (
+			<div className="error">
+				You are trying to enter the magical wonderland.
+				I'm sorry, but unfortunately it does not exist.
+			</div>
+		);
+	}
+}
+
+export default Navbar;
