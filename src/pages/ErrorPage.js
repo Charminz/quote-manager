e7f23b7c54d81179bf27374bbeda6e8381b8710a
charminz <indrek.toomsalu@gmail.com>
2019-02-17 21:47:24 +0200

    Added error page. Style fixes.

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import errorImg from "../assets/error-grave.png";
 
 class Navbar extends Component {
 	render() {
 		return (
 			<div className="error">
-				You are trying to enter the magical wonderland.
-				I'm sorry, but unfortunately it does not exist.
+				<div>
+					You are trying to enter the magical wonderland.
+					I'm sorry, but unfortunately it does not exist.
+				</div>
+				<img src={errorImg} alt="Invalid page"/>
 			</div>
 		);
 	}
