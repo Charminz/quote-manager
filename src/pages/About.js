6b45cf869081485ea9507c2b4cf3d5936a334745
charminz <indrek.toomsalu@gmail.com>
2019-02-17 22:01:02 +0200

    Added about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,25 @@
 import React, {Component} from 'react'
+import linkedin from "../assets/linkedin-img.svg";
+import github from "../assets/github-img.svg";
 
 class About extends Component {
 	render() {
 		return (
-			<div>
-				About
+			<div className="about">
+				<div className="about-me">
+					<h2>Indrek Toomsalu</h2>
+					<p>I'm passionate frontend developer with mostly experience in Vue.js.</p>
+					<p>I am also a Informatics student at Tallinn University of Technology, where I'm finishing a bachelor's degree.</p>
+				</div>
+				<div className="social">
+					<h4>Find more information about me at:</h4>
+					<a target="_blank" href="https://www.linkedin.com/in/indrek-toomsalu/">
+						<img src={linkedin} className="social-icon"/>
+					</a>
+					<a target="_blank" href="https://github.com/Charminz">
+						<img src={github} className="social-icon"/>
+					</a>
+				</div>
 			</div>
 		);
 	}
